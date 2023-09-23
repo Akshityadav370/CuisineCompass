d2659b757e1ea81e7dd04208f78a5a5ce11d8d90
Akshit Yadav <akshit07032001@gmail.com>
2023-09-23 11:57:23 +0530

    orders history done!

--- cuisinecompass/src/App.js
+++ cuisinecompass/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./screens/Login";
 import SignUp from "./screens/SignUp";
 import { CartProvider } from "./components/ContextReducer";
+import MyOrder from "./screens/MyOrder";
 // import '../node_modules/bootstrap-dark-5/dist/css/boo';
 // // import '../node_modules/bootstrap/dist/js/bootstrap-bundle';
 // import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
@@ -15,6 +16,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/createuser" element={<SignUp />} />
+        <Route exact path="/myOrder" element={<MyOrder />} />
       </Routes>
     </CartProvider>
   );
