45fb8129568dc6593a20ed84317ee43ce2d95e83
Akshit Yadav <akshit07032001@gmail.com>
2023-09-23 07:46:16 +0530

    add to cart functionality completed!

--- cuisinecompass/src/App.js
+++ cuisinecompass/src/App.js
@@ -1,23 +1,22 @@
-
-import './App.css';
-import Home from './screens/Home';
-import {
-  Routes,
-  Route,
-} from 'react-router-dom';
-import Login from './screens/Login';
-import SignUp from './screens/SignUp';
+import "./App.css";
+import Home from "./screens/Home";
+import { Routes, Route } from "react-router-dom";
+import Login from "./screens/Login";
+import SignUp from "./screens/SignUp";
+import { CartProvider } from "./components/ContextReducer";
 // import '../node_modules/bootstrap-dark-5/dist/css/boo';
 // // import '../node_modules/bootstrap/dist/js/bootstrap-bundle';
 // import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
 function App() {
   return (
+    <CartProvider>
       <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route exact path='/login' element={<Login/>}/>
-        <Route exact path='/createuser' element={<SignUp/>}/>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/createuser" element={<SignUp />} />
       </Routes>
+    </CartProvider>
   );
 }
 
--- cuisinecompass/src/components/Navbar.js
+++ cuisinecompass/src/components/Navbar.js
@@ -1,7 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Modal from "../Modal";
+import Badge from "react-bootstrap/Badge";
+import Cart from "../screens/Cart";
+import { useCart } from "./ContextReducer";
 
 export default function Navbar() {
+  const [cartView, setCartView] = useState(false);
+  const navigate = useNavigate();
+  const data = useCart();
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    navigate("/login");
+  };
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success">
@@ -21,25 +33,65 @@ export default function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav me-auto mb-1">
               <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/">
+                <Link
+                  className="nav-link active fs-5"
+                  aria-current="page"
+                  to="/"
+                >
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
+              {localStorage.getItem("authToken") ? (
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active fs-5"
+                    aria-current="page"
+                    to="/"
+                  >
+                    My orders
+                  </Link>
+                </li>
+              ) : (
+                ""
+              )}
+            </ul>
+            {!localStorage.getItem("authToken") ? (
+              <div className="d-flex">
+                <Link className="btn bg-white text-success mx-1" to="/login">
                   Login
                 </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/createuser">
+
+                <Link
+                  className="btn bg-white text-success mx-1"
+                  to="/createuser"
+                >
                   SignUp
                 </Link>
-              </li>
-              
-              
-            </ul>
+              </div>
+            ) : (
+              <>
+                <div
+                  className="btn bg-white text-success mx-1"
+                  onClick={() => setCartView(true)}
+                >
+                  My Cart{" "}
+                  <Badge pill bg="danger">
+                    {data ? data.length : 0}
+                  </Badge>
+                </div>
+                {cartView ? (
+                  <Modal onClose={() => setCartView(false)}><Cart/></Modal>
+                ) : null}
+                <div
+                  className="btn bg-white text-danger mx-1"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </div>
+              </>
+            )}
           </div>
         </div>
       </nav>
